2f71173172ab816e3f1e0901353db3bdee72e33f
TFQ_Moon <no1msh1217@gmail.com>
2022-02-26 14:12:07 +0900

    VIEW 액션 기능 사용 (playstore)

--- app/src/main/java/com/example/intent_20220226/MainActivity.kt
+++ app/src/main/java/com/example/intent_20220226/MainActivity.kt
@@ -86,6 +86,13 @@ class MainActivity : AppCompatActivity() {
             val myIntent = Intent(Intent.ACTION_VIEW , myUri)
             startActivity(myIntent)
         }
+
+        btnKakaoAppLink.setOnClickListener {
+
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent(Intent.ACTION_VIEW , myUri)
+            startActivity(myIntent)
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
