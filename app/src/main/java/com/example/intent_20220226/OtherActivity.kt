081acb9ab24ee31f963378bb856f98bc23d76cf3
TFQ_Moon <no1msh1217@gmail.com>
2022-02-26 11:14:45 +0900

    액티비티 종료는 finish()

--- app/src/main/java/com/example/intent_20220226/OtherActivity.kt
+++ app/src/main/java/com/example/intent_20220226/OtherActivity.kt
@@ -1,11 +1,23 @@
 package com.example.intent_20220226
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        btnReturn.setOnClickListener {
+
+//            이전화면 ( 메인 화면 ) 으로 복귀
+
+//            val myIntent = Intent(this, MainActivity :: class.java )
+//            startActivity(myIntent)
+
+            finish()
+        }
     }
 }
