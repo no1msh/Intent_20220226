bd1e265c4a72ab81e73f8ad78365af04037767b4
TFQ_Moon <no1msh1217@gmail.com>
2022-02-26 10:52:24 +0900

    다른화면 이동하는 기능 추가

--- app/src/main/java/com/example/intent_20220226/MainActivity.kt
+++ app/src/main/java/com/example/intent_20220226/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.intent_20220226
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,7 +13,8 @@ class MainActivity : AppCompatActivity() {
         btnMoveToOther.setOnClickListener {
 
 //            OtherActivity로 이동
-
+            val myIntent = Intent(this , OtherActivity::class.java)
+            startActivity(myIntent)
         }
     }
 }
