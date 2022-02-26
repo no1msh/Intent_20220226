70b6bf35c0dd5eaa795aabba7a1d39424a67c251
TFQ_Moon <no1msh1217@gmail.com>
2022-02-26 10:48:19 +0900

    프로젝트 기본세팅 완료 + 다른화면 이동하는 버튼 추가

--- app/src/main/java/com/example/intent_20220226/MainActivity.kt
+++ app/src/main/java/com/example/intent_20220226/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.intent_20220226
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnMoveToOther.setOnClickListener {
+
+//            OtherActivity로 이동
+
+        }
     }
 }
